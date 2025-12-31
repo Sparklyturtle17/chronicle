8fd74aa1fbfde2ca7b3a74a99fb3daaf4e457475
Sparklyturtle17 <rafiki.erickson@gmail.com>
2025-12-31 22:13:07 +0000

    settings warning

--- src/Title.jsx
+++ src/Title.jsx
@@ -4,11 +4,12 @@ export default function Title() {
       <div className="flex-shrink-0">
         <img src="src/Malawi-Map.png" alt="Map of Malawi" className="h-24 w-auto" />
       </div>
-      <div className="flex-grow flex flex-col items-center justify-center relative">
+      <div className="flex-grow flex flex-col items-center relative">
         <h1 className="text-7xl font-bold">Adventures in Malawi!</h1>
         <div className="absolute bottom-0 right-0 text-right text-lg">
           <p>If you already have Mom's phone number,</p>
           <p>you can download whatsapp to message her.</p>
+          <p className="text-xs">-you might want to turn off auto-download in the settings.</p>
         </div>
       </div>
     </div>
