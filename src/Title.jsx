4f791a83dc92f31ee8db038a9a132908dadc96c7
Sparklyturtle17 <rafiki.erickson@gmail.com>
2025-12-31 22:06:48 +0000

    switched to html entries, styled title bar, mom's contact

--- src/Title.jsx
+++ src/Title.jsx
@@ -1,9 +1,16 @@
 export default function Title() {
   return (
-    <div>
-      <span>Adventures in Malawi!
-        <img src="src/Malawi-Map.png" alt="Map of Malawi"/>
-      </span>
+    <div className="flex items-stretch gap-4">
+      <div className="flex-shrink-0">
+        <img src="src/Malawi-Map.png" alt="Map of Malawi" className="h-24 w-auto" />
+      </div>
+      <div className="flex-grow flex flex-col items-center justify-center relative">
+        <h1 className="text-7xl font-bold">Adventures in Malawi!</h1>
+        <div className="absolute bottom-0 right-0 text-right text-lg">
+          <p>If you already have Mom's phone number,</p>
+          <p>you can download whatsapp to message her.</p>
+        </div>
+      </div>
     </div>
   )
 }
